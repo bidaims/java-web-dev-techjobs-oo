a8fc317be29766c8a3da5689030e2897482b218d
abidemi ajidahun <a_ajidahun@yahoo.com>
2021-02-04 20:00:43 -0600

    Initial commit for Assignment 2-Java

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -12,7 +12,7 @@ public class CoreCompetency {
         nextId++;
     }
 
-    public CoreCompetency(String value) {
+        public CoreCompetency(String value) {
         this();
         this.value = value;
     }
@@ -39,4 +39,17 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
